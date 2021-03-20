93ba0068602484bac44db4f9e5f7cafb91d2c809
sourjp <hiro0209.ki@gmail.com>
2021-03-20 23:08:34 +0900

    Update test cases

--- tests/test_rename.py
+++ tests/test_rename.py
@@ -1,5 +1,4 @@
 import os
-import tempfile
 import pathlib
 
 from renamer.rename import FileRenamer
@@ -11,25 +10,28 @@ class TestFileRenamer:
     def setup_class(cls):
         cls.file_renamer = FileRenamer()
 
-    def test_rename(self):
-        with tempfile.TemporaryDirectory() as tmpdir:
-            src_file_path = os.path.join(tmpdir, 'file1.txt')
-            dst_file_path = os.path.join(tmpdir, 'file2.txt')
-            pathlib.Path(src_file_path).touch()
-
-            self.file_renamer.rename(src_file_path, dst_file_path)
-
-            assert os.path.exists(src_file_path) is False
-            assert os.path.exists(dst_file_path) is True
+    def test_rename(self, tmpdir):
+        src_file_path = os.path.join(tmpdir, 'file1.txt')
+        dst_file_path = os.path.join(tmpdir, 'file2.txt')
+        pathlib.Path(src_file_path).touch()
+        self.file_renamer.rename(src_file_path, dst_file_path)
+        assert os.path.exists(src_file_path) is False
+        assert os.path.exists(dst_file_path) is True
 
     def test_name_as(self):
-        case = ('./tmp/file1.jpg', 'file2')
-        dst_path = self.file_renamer.name_as(case[0], case[1])
-
-        assert dst_path == './tmp/file2.jpg'
+        test_case = {
+            'src': './tmp/file1.jpg',
+            'dst': 'file2',
+            'want': './tmp/file2.jpg'}
+        dst_path = self.file_renamer.name_as(test_case['src'],
+                                             test_case['dst'])
+        assert dst_path == test_case['want']
 
     def test_get_file_path(self):
-        case = './tmp/file1.jpg'
-        dir_name, file_name, ext_name = self.file_renamer._get_file_path(case)
+        test_case = {
+            'file': './tmp/file1.jpg',
+            'want': ('./tmp', 'file1', '.jpg')}
 
-        assert (dir_name, file_name, ext_name) == ('./tmp', 'file1', '.jpg')
+        dir_name, file_name, ext_name = self.file_renamer._get_file_path(
+            test_case['file'])
+        assert (dir_name, file_name, ext_name) == test_case['want']
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,6 +1,7 @@
 import tempfile
 import pathlib
 import os
+import shutil
 
 from renamer.search import FileSearcher
 
@@ -10,27 +11,34 @@ class TestFileSearcher:
     @classmethod
     def setup_class(cls):
         cls.file_searcher = FileSearcher()
+        cls.tmpdir = tempfile.mkdtemp()
+        cls.file1_path = os.path.join(cls.tmpdir, 'file1.txt')
+        cls.file2_path = os.path.join(cls.tmpdir, 'tmp', 'file2.txt')
+        cls.keyword = os.path.join(cls.tmpdir, '**')
+
+        os.makedirs(os.path.join(cls.tmpdir, 'tmp'))
+        pathlib.Path(cls.file1_path).touch()
+        pathlib.Path(cls.file2_path).touch()
+
+    @classmethod
+    def teardown_class(cls):
+        shutil.rmtree(cls.tmpdir)
 
     def test_search(self):
-        with tempfile.TemporaryDirectory() as tempdir:
-            file1_path = os.path.join(tempdir, 'file1.txt')
-            file2_path = os.path.join(tempdir, 'tmp', 'file2.txt')
-            os.makedirs(os.path.join(tempdir, 'tmp'))
-            pathlib.Path(file1_path).touch()
-            pathlib.Path(file2_path).touch()
-
-            key = os.path.join(tempdir, '**')
-            result = self.file_searcher.search(key)
-            assert result == [file1_path]
+        result = self.file_searcher.search(self.keyword)
+        assert result == [self.file1_path]
 
     def test_search_recursive(self):
-        with tempfile.TemporaryDirectory() as tempdir:
-            file1_path = os.path.join(tempdir, 'file1.txt')
-            file2_path = os.path.join(tempdir, 'tmp', 'file2.txt')
-            os.makedirs(os.path.join(tempdir, 'tmp'))
-            pathlib.Path(file1_path).touch()
-            pathlib.Path(file2_path).touch()
+        result = self.file_searcher.search(self.keyword, recursive=True)
+        assert result == [self.file1_path, self.file2_path]
 
-            key = os.path.join(tempdir, '**')
-            result = self.file_searcher.search(key, recursive=True)
-            assert result == [file1_path, file2_path]
+    def test_select_file(self):
+        test_case = {
+            'files': [
+                os.path.join(self.tmpdir, 'file1.txt'),
+                os.path.join(self.tmpdir, 'tmp')],
+            'want': [
+                os.path.join(self.tmpdir, 'file1.txt')]}
+
+        result = self.file_searcher._select_file(test_case['files'])
+        assert result == test_case['want']
